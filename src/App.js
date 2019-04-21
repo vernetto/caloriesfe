9a2c24de64b34c43ca7025aa0da433716b6a8ab8
vernetto <publicpierre@gmail.com>
2019-04-21 22:53:51 +0200

    material

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   state = {
@@ -31,6 +32,11 @@ class App extends Component {
                   {group.name}
                 </div>
               )}
+            </div>
+            <div>
+              <Button variant="contained" color="primary">
+                  Hello World
+              </Button>
             </div>
           </header>
         </div>
